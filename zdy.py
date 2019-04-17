e8c3b201c72cbfa6401860fcd9fc0a49f11ccd9c
xNathan <i@denghw.cn>
2019-04-18 01:00:14 +0800

    style: format with `black` and sort imports

--- zdy.py
+++ zdy.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 # zdy.py
-'''
+"""
 Squid+站大爷搭建代理IP池
-Author: Nathan
-Blog: www.xnathan.com
-Github: github.com/xNathan
-'''
-from gevent import monkey
-monkey.patch_all()
+Author: xNathan
+Blog: https://xnathan.com
+Github: https://github.com/xNathan
+"""
+from gevent import monkey  # isort:skip
 
+monkey.patch_all()  # isort:skip
+
+import logging
 import os
 import time
-import logging
+
 import requests
 from gevent.pool import Pool
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s: - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
+    "%(asctime)s - %(name)s - %(levelname)s: - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 # 使用StreamHandler输出到屏幕
 ch = logging.StreamHandler()
@@ -28,7 +30,6 @@ ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
 
 logger.addHandler(ch)
-
 
 # Squid的配置文件语法
 # 将请求转发到父代理
@@ -39,31 +40,30 @@ GOOD_PROXIES = []
 
 pool = Pool(50)
 
+
 def check_proxy(proxy):
     """验证代理是否可用
     :param proxy list:[ip, port]"""
     global GOOD_PROXIES
     ip, port = proxy
-    _proxies = {
-        'http': '{}:{}'.format(ip, port)
-    }
+    _proxies = {"http": "{}:{}".format(ip, port)}
     try:
-        res = requests.get(
-            'http://1212.ip138.com/ic.asp', proxies=_proxies, timeout=10)
+        ip_url = "http://1212.ip138.com/ic.asp"
+        res = requests.get(ip_url, proxies=_proxies, timeout=10)
         assert ip in res.content
-        logger.info('[GOOD] - {}:{}'.format(ip, port))
+        logger.info("[GOOD] - {}:{}".format(ip, port))
         GOOD_PROXIES.append(proxy)
-    except Exception, e:
-        logger.error('[BAD] - {}:{}, {}'.format(ip, port, e))
+    except Exception as e:
+        logger.error("[BAD] - {}:{}, {}".format(ip, port, e))
 
 
 def update_conf():
-    with open('/etc/squid/squid.conf.original', 'r') as F:
+    with open("/etc/squid/squid.conf.original", "r") as F:
         squid_conf = F.readlines()
-    squid_conf.append('\n# Cache peer config\n')
+    squid_conf.append("\n# Cache peer config\n")
     for proxy in GOOD_PROXIES:
         squid_conf.append(PEER_CONF % (proxy[0], proxy[1]))
-    with open('/etc/squid/squid.conf', 'w') as F:
+    with open("/etc/squid/squid.conf", "w") as F:
         F.writelines(squid_conf)
 
 
@@ -71,24 +71,24 @@ def get_proxy():
     global GOOD_PROXIES
     GOOD_PROXIES = []
     # 1. 获取代理IP资源
-    api_url = 'http://s.zdaye.com/?api=YOUR_API&count=100&fitter=1&px=2'
+    api_url = "http://s.zdaye.com/?api=YOUR_API&count=100&fitter=1&px=2"
     res = requests.get(api_url).content
     if len(res) == 0:
-        logger.error('no data')
-    elif 'bad' in res:
-        logger.error('bad request')
+        logger.error("no data")
+    elif "bad" in res:
+        logger.error("bad request")
     else:
-        logger.info('get all proxies')
+        logger.info("get all proxies")
         proxies = []
         for line in res.split():
-            proxies.append(line.strip().split(':'))
+            proxies.append(line.strip().split(":"))
         pool.map(check_proxy, proxies)
         pool.join()
         # 2. 写入Squid配置文件
         update_conf()
         # 3. 重新加载配置文件
-        os.system('squid -k reconfigure')
-        logger.info('>>>> DONE! <<<<')
+        os.system("squid -k reconfigure")
+        logger.info(">>>> DONE! <<<<")
 
 
 def main():
@@ -101,5 +101,5 @@ def main():
         time.sleep(5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
