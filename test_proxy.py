4186451dd460e00acc7b96ffe0040d64d1d8d7fa
xNathan <i@denghw.cn>
2019-04-18 00:59:23 +0800

    style: support py3

--- test_proxy.py
+++ test_proxy.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-'''
+"""
 代理IP池实例演示
-'''
+"""
+from __future__ import print_function
+
 import requests
+
 s = requests.Session()
-s.proxies.update(
-    {'http': '139.xxx.xxx.66:3188'}
-)
-print s.get('http://httpbin.org/ip')
+s.proxies.update({"http": "139.xxx.xxx.66:3188"})
+print(s.get("http://httpbin.org/ip"))
